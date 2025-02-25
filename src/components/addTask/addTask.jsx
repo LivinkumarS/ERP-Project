8f384645ad56d460d921b0578e86a7700dc24276
LivinkumarS <001liv100@ggmail.com>
2025-02-25 15:38:22 +0530

    Updated Task

--- src/components/addTask/addTask.jsx
+++ src/components/addTask/addTask.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./addTask.css";
+import { toast } from "react-toastify";
 
 export default function addTask({
   setShowForm,
@@ -45,6 +46,10 @@ export default function addTask({
   function handleSubmit(e) {
     e.preventDefault();
 
+    if(currentForm.name==="" || currentForm.assigned_to==="" || currentForm.due_date==="" || currentForm.start_date==="" || currentForm.priority==="" || currentForm.status===""){
+      return toast.warn("Please Fillout All The Fields")
+    }
+
     if (formCall === "add") {
       AddNewTask(currentForm);
     } else {
@@ -65,88 +70,116 @@ export default function addTask({
 
   return (
     <div className="addTask-container">
-      <svg
-        onClick={() => {
-          setCurrentForm({
-            name: "",
-            status: "",
-            start_date: "",
-            due_date: "",
-            assigned_to: "",
-            priority: "",
-          });
-          setShowForm(false);
-        }}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 384 512"
-      >
-        <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
-      </svg>
+      <div className="addtask-layout">
+        <svg
+          onClick={() => {
+            setCurrentForm({
+              name: "",
+              status: "",
+              start_date: "",
+              due_date: "",
+              assigned_to: "",
+              priority: "",
+            });
+            setShowForm(false);
+          }}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 384 512"
+        >
+          <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+        </svg>
 
-      <form>
-        <input
-          type="text"
-          placeholder="name"
-          id="name"
-          required
-          onChange={handleValueChange}
-          value={currentForm.name}
-        />
-        <input
-          type="text"
-          placeholder="status"
-          id="status"
-          required
-          onChange={handleValueChange}
-          value={currentForm.status}
-        />
-        <input
-          type="date"
-          placeholder="Start Date"
-          id="start_date"
-          required
-          onChange={handleDateChange}
-          defaultValue={
-            currentForm.start_date && currentForm.start_date !== ""
-              ? new Date(currentForm.start_date).toISOString().split("T")[0]
-              : ""
-          }
-        />
+        <form className="addtask-form">
+          <div>
+            <nav>Name</nav>
+            <input
+              type="text"
+              id="name"
+              required
+              onChange={handleValueChange}
+              value={currentForm.name}
+            />
+          </div>
+          <div>
+            <nav>Status</nav>
+            <select
+              id="status"
+              required
+              onChange={handleValueChange}
+              value={currentForm.status}
+            >
+              <option value="" disabled>
+                Select Status
+              </option>
+              <option value="Not Started">Not Started</option>
+              <option value="In Progress">In Progress</option>
+              <option value="Completed">Completed</option>
+              <option value="Awaiting Feedback">Awaiting Feedback</option>
+            </select>
+          </div>
 
-        <input
-          type="date"
-          placeholder="Due Date"
-          id="due_date"
-          required
-          onChange={handleDateChange}
-          defaultValue={
-            currentForm.due_date && currentForm.due_date !== ""
-              ? new Date(currentForm.due_date).toISOString().split("T")[0]
-              : ""
-          }
-        />
+          <div>
+            <nav>Start Data</nav>
+            <input
+              type="date"
+              id="start_date"
+              required
+              onChange={handleDateChange}
+              defaultValue={
+                currentForm.start_date && currentForm.start_date !== ""
+                  ? new Date(currentForm.start_date).toISOString().split("T")[0]
+                  : ""
+              }
+            />
+          </div>
+          <div>
+            <nav>Due Date</nav>
+            <input
+              type="date"
+              id="due_date"
+              required
+              onChange={handleDateChange}
+              defaultValue={
+                currentForm.due_date && currentForm.due_date !== ""
+                  ? new Date(currentForm.due_date).toISOString().split("T")[0]
+                  : ""
+              }
+            />
+          </div>
+          <div>
+            <nav>Assigned to</nav>
+            <input
+              type="text"
+              id="assigned_to"
+              required
+              onChange={handleValueChange}
+              value={currentForm.assigned_to}
+            />
+          </div>
 
-        <input
-          type="text"
-          placeholder="assigned to"
-          id="assigned_to"
-          required
-          onChange={handleValueChange}
-          value={currentForm.assigned_to}
-        />
-        <input
-          type="text"
-          placeholder="priority"
-          id="priority"
-          required
-          onChange={handleValueChange}
-          value={currentForm.priority}
-        />
-
-        <button onClick={handleSubmit} type="submit">
-          {formCall === "add" ? "submit" : "edit"}
-        </button>
-      </form>
+          <div>
+            <nav>Priority</nav>
+            <select
+              id="priority"
+              required
+              onChange={handleValueChange}
+              value={currentForm.priority}
+            >
+              <option value="" disabled>
+                Select Priority
+              </option>
+              <option value="Low">Low</option>
+              <option value="Medium">Medium</option>
+              <option value="High">High</option>
+            </select>
+          </div>
+        </form>
+        <div>
+          <button onClick={handleSubmit} type="submit">
+            {formCall === "add" ? "Submit" : "Edit"}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/task/task.jsx
+++ src/components/task/task.jsx
@@ -7,6 +7,7 @@ export default function task() {
   const [apiData, setApiData] = useState({});
   const [taskData, setTaskData] = useState([]);
   const [taskSummary, setTaskSummary] = useState([]);
+  const [showFilter, setShowFilter] = useState(false);
 
   const [showForm, setShowForm] = useState(false);
   const [formCall, setFormCall] = useState("");
@@ -105,14 +106,32 @@ export default function task() {
           New Task
         </button>
         <div className="taskright-cointainer">
-          <button className="task-overview">Task Overview</button>
-          <svg
-            className="filter-logo"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-            <path d="M3.9 54.9C10.5 40.9 24.5 32 40 32l432 0c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9 320 448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6l0-79.1L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z" />
-          </svg>
+          <div className="filter-cont">
+            <svg
+              onClick={() => setShowFilter((prev) => !prev)}
+              className="filter-logo"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path d="M3.9 54.9C10.5 40.9 24.5 32 40 32l432 0c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9 320 448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6l0-79.1L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z" />
+            </svg>
+
+            {showFilter && (
+              <div className="filter-contents">
+                <form>
+                  <h4>Status</h4>
+                  <select id="status" name="status">
+                    <option value="not_started">Not Started</option>
+                    <option value="in_progress">In Progress</option>
+                    <option value="completed">Completed</option>
+                    <option value="awaiting_feedback">Awaiting Feedback</option>
+                  </select>
+
+                  <button type="submit">Apply Filters</button>
+                </form>
+              </div>
+            )}
+          </div>
         </div>
       </div>
       <div className="bar-line"></div>
